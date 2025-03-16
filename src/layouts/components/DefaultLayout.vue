8e30a7c15514030174742a55e445b79ee5213a18
CoinSeyir <coinseyir@gmail.com>
2025-03-16 15:43:17 +0300

    Update DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -1,88 +1,14 @@
-<script lang="ts" setup>
-import { Icon } from '@iconify/vue';
-import { computed, ref } from 'vue';
-
-// Components
-import newFooter from '@/layouts/components/NavFooter.vue';
-import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue';
-import NavbarSearch from '@/layouts/components/NavbarSearch.vue';
-import ChainProfile from '@/layouts/components/ChainProfile.vue';
-import Sponsors from '@/layouts/components/Sponsors.vue';
-
-import { NetworkType, useDashboard } from '@/stores/useDashboard';
-import { useBaseStore, useBlockchain } from '@/stores';
-
-import NavBarI18n from './NavBarI18n.vue';
-import NavBarWallet from './NavBarWallet.vue';
-import type { NavGroup, NavLink, NavSectionTitle, VerticalNavItems } from '../types';
-import dayjs from 'dayjs';
-
-const dashboard = useDashboard();
-dashboard.initial();
-const blockchain = useBlockchain();
-blockchain.randomSetupEndpoint();
-const baseStore = useBaseStore();
-
-const current = ref(''); // the current chain
-const temp = ref('')
-blockchain.$subscribe((m, s) => {
-  if(current.value ===s.chainName && temp.value != s.endpoint.address) {
-    temp.value = s.endpoint.address
-    blockchain.initial();
-  }
-  if (current.value != s.chainName) {
-    current.value = s.chainName;
-    blockchain.randomSetupEndpoint();
-  }
-});
-
-const sidebarShow = ref(false);
-const sidebarOpen = ref(true);
-
-const changeOpen = (index: Number) => {
-  if (index === 0) {
-    sidebarOpen.value = !sidebarOpen.value;
-  }
-};
-const showDiscord = window.location.host.search('ping.pub') > -1;
-
-function isNavGroup(nav: VerticalNavItems | any): nav is NavGroup {
-   return (<NavGroup>nav).children !== undefined;
-}
-function isNavLink(nav: VerticalNavItems | any): nav is NavLink {
-   return (<NavLink>nav).to !== undefined;
-}
-function isNavTitle(nav: VerticalNavItems | any): nav is NavSectionTitle {
-   return (<NavSectionTitle>nav).heading !== undefined;
-}
-function selected(route: any, nav: NavLink) {
-  const b = route.path === nav.to?.path || route.path.startsWith(nav.to?.path) && nav.title.indexOf('dashboard') === -1
-  return b
-}
-const blocktime = computed(() => {
-  return dayjs(baseStore.latest?.block?.header?.time)
-});
-
-const behind = computed(() => {
-  const current = dayjs().subtract(10, 'minute')
-  return blocktime.value.isBefore(current)
-});
-
-dayjs()
-
-</script>
-
 <template>
-  <div class="bg-gray-100 dark:bg-[#171d30]">
+  <div class="bg-gray-50 dark:bg-gray-900">
     <!-- sidebar -->
     <div
-      class="w-64 fixed z-50 left-0 top-0 bottom-0 overflow-auto bg-base-100 border-r border-gray-100 dark:border-gray-700"
+      class="w-64 fixed z-50 left-0 top-0 bottom-0 overflow-auto bg-white dark:bg-gray-800 border-r border-gray-200 dark:border-gray-700"
       :class="{ block: sidebarShow, 'hidden xl:!block': !sidebarShow }"
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
           <img class="w-10 h-10" src="../../assets/logo.svg" />
-          <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
+          <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-gray-100">
             Ping.pub
           </h1>
         </RouterLink>
@@ -115,15 +41,15 @@ dayjs()
             @click="changeOpen(index)"
           />
           <div
-            class="collapse-title !py-0 px-4 flex items-center cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
+            class="collapse-title !py-0 px-4 flex items-center cursor-pointer hover:bg-blue-50 dark:hover:bg-blue-900"
           >
             <Icon
               v-if="item?.icon?.icon"
               :icon="item?.icon?.icon"
               class="text-xl mr-2"
               :class="{
-                'text-yellow-500': item?.title === 'Favorite',
-                'text-blue-500': item?.title !== 'Favorite',
+                'text-orange-500': item?.title === 'Favorite',
+                'text-indigo-500': item?.title !== 'Favorite',
               }"
             />
             <img
@@ -132,7 +58,7 @@ dayjs()
               class="w-6 h-6 rounded-full mr-3"
             />
             <div
-              class="text-base capitalize flex-1 text-gray-700 dark:text-gray-200 whitespace-nowrap"
+              class="text-base capitalize flex-1 text-gray-800 dark:text-gray-100 whitespace-nowrap"
             >
               {{ item?.title }}
             </div>
@@ -145,13 +71,13 @@ dayjs()
             </div>
           </div>
           <div class="collapse-content">            
-            <div v-for="(el, key) of item?.children" class="menu bg-base-100 w-full !p-0">
+            <div v-for="(el, key) of item?.children" class="menu bg-white dark:bg-gray-800 w-full !p-0">
               <RouterLink
                 v-if="isNavLink(el)"
                 @click="sidebarShow = false"
-                class="hover:bg-gray-100 dark:hover:bg-[#373f59] rounded cursor-pointer px-3 py-2 flex items-center"
+                class="hover:bg-blue-50 dark:hover:bg-blue-900 rounded cursor-pointer px-3 py-2 flex items-center"
                 :class="{
-                  '!bg-primary': selected($route, el),
+                  '!bg-indigo-500': selected($route, el),
                 }"
                 :to="el.to"
               >
@@ -173,7 +99,7 @@ dayjs()
                 }"
                 />
                 <div
-                  class="text-base capitalize text-gray-500 dark:text-gray-300"
+                  class="text-base capitalize text-gray-600 dark:text-gray-300"
                   :class="{
                     '!text-white': selected($route, el),
                   }"
@@ -182,21 +108,21 @@ dayjs()
                 </div>
               </RouterLink>
             </div>
-            <div v-if="index === 0 && dashboard.networkType === NetworkType.Testnet" class="menu bg-base-100 w-full !p-0">
+            <div v-if="index === 0 && dashboard.networkType === NetworkType.Testnet" class="menu bg-white dark:bg-gray-800 w-full !p-0">
               <RouterLink 
-              class="hover:bg-gray-100 dark:hover:bg-[#373f59] rounded cursor-pointer px-3 py-2 flex items-center"
+              class="hover:bg-blue-50 dark:hover:bg-blue-900 rounded cursor-pointer px-3 py-2 flex items-center"
               :to="`/${blockchain.chainName}/faucet`">
                 <Icon
                   icon="mdi:chevron-right"
                   class="mr-2 ml-3"
                   ></Icon>
                 <div
-                  class="text-base capitalize text-gray-500 dark:text-gray-300"
+                  class="text-base capitalize text-gray-600 dark:text-gray-300"
                 >
                   Faucet
                 </div>
                 <div
-                  class="badge badge-sm text-white border-none badge-error ml-auto" 
+                  class="badge badge-sm text-white border-none bg-red-500 ml-auto" 
                 >
                   New
                 </div>
@@ -209,24 +135,24 @@ dayjs()
           v-if="isNavLink(item)"
           :to="item?.to"
           @click="sidebarShow = false"
-          class="cursor-pointer rounded-lg px-4 flex items-center py-2 hover:bg-gray-100 dark:hover:bg-[#373f59]"
+          class="cursor-pointer rounded-lg px-4 flex items-center py-2 hover:bg-blue-50 dark:hover:bg-blue-900"
         >
           <Icon
             v-if="item?.icon?.icon"
             :icon="item?.icon?.icon"
             class="text-xl mr-2"
             :class="{
-              'text-yellow-500': item?.title === 'Favorite',
-              'text-blue-500': item?.title !== 'Favorite',
+              'text-orange-500': item?.title === 'Favorite',
+              'text-indigo-500': item?.title !== 'Favorite',
             }"
           />
           <img
             v-if="item?.icon?.image"
             :src="item?.icon?.image"
-            class="w-6 h-6 rounded-full mr-3 border border-blue-100"
+            class="w-6 h-6 rounded-full mr-3 border border-indigo-100"
           />
           <div
-            class="text-base capitalize flex-1 text-gray-700 dark:text-gray-200 whitespace-nowrap"
+            class="text-base capitalize flex-1 text-gray-800 dark:text-gray-100 whitespace-nowrap"
           >
             {{ item?.title }}
           </div>
@@ -240,38 +166,38 @@ dayjs()
         </RouterLink>
         <div
           v-if="isNavTitle(item)"
-          class="px-4 text-sm text-gray-400 pb-2 uppercase"
+          class="px-4 text-sm text-gray-500 pb-2 uppercase"
         >
           {{ item?.heading }}
         </div>
       </div>
       <div class="px-2">
-          <div class="px-4 text-sm pt-2 text-gray-400 pb-2 uppercase">
+          <div class="px-4 text-sm pt-2 text-gray-500 pb-2 uppercase">
             Tools
           </div>
           <RouterLink to="/wallet/suggest"
-          class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
+          class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-blue-50 dark:hover:bg-blue-900"
           >
             <Icon icon="mdi:frequently-asked-questions" class="text-xl mr-2" />
             <div
-              class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+              class="text-base capitalize flex-1 text-gray-600 dark:text-gray-100"
             >
               Wallet Helper
             </div>
           </RouterLink>
-          <div class="px-4 text-sm pt-2 text-gray-400 pb-2 uppercase">
+          <div class="px-4 text-sm pt-2 text-gray-500 pb-2 uppercase">
           {{ $t('module.sponsors') }}
         </div>
         <Sponsors />
-        <div class="px-4 text-sm pt-2 text-gray-400 pb-2 uppercase">{{ $t('module.links') }}</div>
+        <div class="px-4 text-sm pt-2 text-gray-500 pb-2 uppercase">{{ $t('module.links') }}</div>
         <a
           href="https://twitter.com/ping_pub"
           target="_blank"
-          class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
+          class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-blue-50 dark:hover:bg-blue-900"
         >
           <Icon icon="mdi:twitter" class="text-xl mr-2" />
           <div
-            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-100"
           >
             Twitter
           </div>
@@ -280,11 +206,11 @@ dayjs()
           v-if="showDiscord"
           href="https://discord.com/invite/CmjYVSr6GW"
           target="_blank"
-          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
+          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-blue-50 dark:hover:bg-blue-900"
         >
           <Icon icon="mdi:discord" class="text-xl mr-2" />
           <div
-            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-100"
           >
             Discord
           </div>
@@ -292,11 +218,11 @@ dayjs()
         <a
           href="https://github.com/ping-pub/explorer/discussions"
           target="_blank"
-          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
+          class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-blue-50 dark:hover:bg-blue-900"
         >
           <Icon icon="mdi:frequently-asked-questions" class="text-xl mr-2" />
           <div
-            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
+            class="text-base capitalize flex-1 text-gray-600 dark:text-gray-100"
           >
             FAQ
           </div>
@@ -306,7 +232,7 @@ dayjs()
     <div class="xl:!ml-64 px-3 pt-4">
       <!-- header -->
       <div
-        class="flex items-center py-3 bg-base-100 mb-4 rounded px-4 sticky top-0 z-10"
+        class="flex items-center py-3 bg-white dark:bg-gray-800 mb-4 rounded px-4 sticky top-0 z-10"
       >
         <div
           class="text-2xl pr-3 cursor-pointer xl:!hidden"
